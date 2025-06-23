c38190c78f63063b0f6c2f187a4a3ea2fed2a064
Rimon Dutta <darklegend12359@gmail.com>
2025-06-23 12:25:07 +0600

    update

--- src/components/stats/Index.js
+++ src/components/stats/Index.js
@@ -11,24 +11,7 @@ function Stats() {
     return undefined;
   }, []);
 
-  useIsomorphicLayoutEffect(() => {
-    if (typeof window !== 'undefined') {
-      stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
-      document.body.appendChild(stats.dom);
-    }
-
-    return () => {
-      stats.dom.remove();
-    };
-  }, [stats]);
-
-  useFrame(() => {
-    stats.begin();
-  }, -Infinity);
-
-  useFrame(() => {
-    stats.end();
-  }, Infinity);
+ 
 
   return null;
 }
